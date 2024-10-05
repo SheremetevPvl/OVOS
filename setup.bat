7db601c81eb280496a7dab80b1fd51e67a853b39
Gallanthy <124916347+SheremetevPvl@users.noreply.github.com>
2024-10-05 13:19:11 +0300

    Setup

---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Клонируем репозиторий
+git clone https://github.com/bradymadden97/2048-game.git
+cd 2048-game
+
+REM Создаем Dockerfile
+echo FROM nginx:alpine > Dockerfile
+echo WORKDIR /usr/share/nginx/html >> Dockerfile
+echo COPY . . >> Dockerfile
+echo EXPOSE 80 >> Dockerfile
+echo CMD ["nginx", "-g", "daemon off;"] >> Dockerfile
+
+REM Сборка Docker-образа
+docker build -t 2048-game .
+
+REM Запуск контейнеров
+docker run -d -p 8080:80 --name 2048-game-1 2048-game
+docker run -d -p 8081:80 --name 2048-game-2 2048-game
