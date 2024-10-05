047edf7f6375093bf8b87ac461039e8fe677409f
Gallanthy <124916347+SheremetevPvl@users.noreply.github.com>
2024-10-05 13:55:18 +0300

    update

--- setup.bat
+++ setup.bat
@@ -17,4 +17,4 @@ docker build -t 2048-game .
 
 REM Запуск контейнеров
 docker run -d -p 8080:80 --name 2048-game-1 2048-game
-docker run -d -p 8081:80 --name 2048-game-2 2048-game
+docker run -d -p 8081:30 --name 2048-game-2 2048-game
